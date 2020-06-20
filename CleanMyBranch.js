60849ab9ec9680f5b9413f63cbeda1b93a6c2f90
Joybell <joybell.kr@gmail.com>
2020-06-20 19:19:56 +0900

    CleanMyBranch

---
+++ CleanMyBranch.js
@@ -0,0 +1,158 @@
+#!/usr/bin/env node
+const axios = require('axios').default
+const shell = require('shelljs')
+const inquirer = require('inquirer') 
+const fs = require('fs')
+const colors = require('colors')
+
+class CleanMyBranch {
+  constructor() {
+    this.encodedAccount = ''
+    this.run()
+  }
+
+  async run() {
+    try {
+      const account = await this.getAccount()
+
+      if (!account) {
+        throw 'JIRA 로그인 실패'
+      }
+
+      this.encodedAccount = new Buffer(`${account.username}:${account.password}`).toString('base64')
+    
+      const projectList = await this.getProjectList()
+      const chooseProject = await this.getProject(projectList)
+      const branchList = await this.getBranchList(chooseProject.project)
+
+      const itemList = branchList.stdout.split('\n').map(x => ({
+        branch: x.replace('*', '').trim(),
+        jira: x.match(/(?<=\/)[A-Z]*\-[0-9]*/g)
+      }))
+
+      itemList.forEach(async (item, index) => {
+        if (index === 0) {
+          this.print('\n[ Jira Issue ]', 'green')
+        }
+
+        let jiraId = item.jira && item.jira[0]       
+        
+        if (jiraId) {        
+          let jiraIssue = await this.getJiraIssue(jiraId)
+
+          if (jiraIssue.errorMessage) {
+            this.print(`${jiraId} => ${jiraIssue.errorMessage}`)
+          } else {
+            let jiraStatus = jiraIssue.data.fields.status.name
+          
+            this.print(`${jiraId} => ${jiraStatus}`)
+  
+            if (['Closed', 'Close', '종료'].includes(jiraStatus)) {
+              this.print(`delete branch: ${item.branch}`, 'red')
+              await this.deleteBranch(item.branch)
+            }
+          }
+        }
+      })
+    } catch (e) {
+      this.print(e, 'red')
+    }
+  }
+
+  async getAccount() {
+    const account = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Jira Username:'
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Jira Password:',
+      }
+    ])  
+
+    try {
+      const result = await axios({
+        method: 'post',
+        url: `http://jira.wemakeprice.com/rest/auth/1/session`,
+        data: {
+          username: account.username,
+          password: account.password
+        }
+      })
+
+      if (result) {
+        return account
+      }
+    } catch (e) {
+      return false
+    }
+  }
+
+  async getProjectList() {
+    return new Promise((resolve, reject) => {
+      const projectList = []
+      
+      shell.cd('../')
+      fs.readdir('./', (err, itemList) => {
+        itemList.forEach(item => {
+          if (!['.git', '.DS_Store', 'cleanmybranch'].includes(item)) {
+            if (fs.lstatSync(item).isDirectory()) {
+              projectList.push(item)
+            }
+          }
+        })
+
+        resolve(projectList)
+      })
+    })
+  }
+
+  async getProject(projectList) {
+    return await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'project',
+        message: 'Choose a Project',
+        choices: [ ...projectList ],
+      },
+    ])
+  }
+
+  async getBranchList(project) {
+    shell.cd(project)
+    this.print('\n[ Branch List ]', 'green')
+    return await shell.exec('git branch')
+  }
+
+  async getJiraIssue(issue) {
+    try {
+      return await axios({
+        method: 'get',
+        url: `http://jira.wemakeprice.com/rest/api/2/issue/${issue}`,
+        headers: {
+          Authorization: `Basic ${this.encodedAccount}`
+        }
+      })
+    } catch (e) {
+      return {
+        errorMessage: e.message
+      }
+    }
+  }
+
+  async deleteBranch(branch) {
+    await shell.exec(`git branch -D ${branch}`)
+    await shell.exec(`git push origin :${branch}`)
+  }
+
+  print(message, color) {
+    console.log(colors[color || 'white'](`${message}`))
+  }
+}
+
+new CleanMyBranch()
+
+
---
+++ README.md
@@ -0,0 +1,17 @@
+# CleanMyBranch
+
+## 개요
+> 로컬 브랜치를 기준으로 지라 이슈의 상태값을 조회하여 종료된 이슈만 로컬/리모트 브랜치를 삭제
+
+## 설치
+> 프로젝트 그룹 폴더에 설치
+> 
+> git clone https://stash.wemakeprice.com/scm/~joybell/cleanmybranch.git
+> 
+> cd cleanmybranch
+> 
+> npm install
+> 
+> chmod +x ./CleanMyBranch.js
+> 
+> ./CleanMyBranch.js
